2291cd887226527f6cdf72ce172dad025493aefe
Daniel Uribe <daniel.londonouribe@southworks.com>
2021-04-14 09:00:27 -0500

    change book income route path

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   namespace :v1 do
     resources :users, only: [:show, :create, :update]
     resources :books, only: [:show, :create]
-    post '/book_income/:id', to: 'books#book_income'
+    post '/books/income/:id', to: 'books#book_income'
     resources :book_transactions, only: [:create, :update]
   end
 end
